c9f2e4ea904ef74f6c42ad87598f0a35bf30098a
DDEssing <david.essing@web.de>
2018-09-13 18:06:38 +0200

    Outsourced functions completely

--- func/def_master.py
+++ func/def_master.py
@@ -3,41 +3,18 @@
 
 
 import math as M
-import numpy as np
-
-def _nearest_pow_2(x):
-    """
-    copied from obspy specgram
-    Find power of two nearest to x
-    >>> _nearest_pow_2(3)
-    2.0
-    >>> _nearest_pow_2(15)
-    16.0
-    :type x: float
-    :param x: Number
-    :rtype: Int
-    :return: Nearest power of 2 to x
-    """
-    a = M.pow(2, M.ceil(np.log2(x)))
-    b = M.pow(2, M.floor(np.log2(x)))
-    if abs(a - x) < abs(b - x):
-        return a
-    else:
-        return b
+import numpy as np 
     
     
-    
-    
-    
-def searchmaxima(maxima, data, distance, scale_vector=0):
+def searchmaxima(maxima, distance, data, scale_vector=0):
     
     '''
     To find maxima in data, with option to give vecotr with scaling values for x-Axis
     
     Input:
     maxima    scalar        value of maxima to find
-    data      1-D array     numpy array of data 
     distance  scalar        value for nearest next maxima to search for
+    data      1-D array     numpy array of data 
     freqvec   1-D array     numpy array of data with values to scale x-Axis (optional)
     
     '''
@@ -72,7 +49,6 @@ def searchmaxima(maxima, data, distance, scale_vector=0):
             
         return(value_max, index_max,value_max_f)
     
-    
 def sorting_out_peaks(input_data, freq_vec, maxima, distance ,threshold, freq_minimal_value):
     '''
     Calls searchmaxima and just hands maxima values which meet with two options:
@@ -82,6 +58,10 @@ def sorting_out_peaks(input_data, freq_vec, maxima, distance ,threshold, freq_mi
     
     '''
     
+    amp_values = np.zeros((np.shape(input_data)[1]))
+    frequency_indices = np.zeros_like(amp_values)
+    frequency_values = np.zeros_like(amp_values)
+    
     
     max_array = np.zeros_like(input_data)
     for j in range(np.shape(input_data)[1]):
@@ -89,14 +69,14 @@ def sorting_out_peaks(input_data, freq_vec, maxima, distance ,threshold, freq_mi
         #if win_average < all_win_average:
         # not a good solution. deletes as well some instrument signal
 
-        amp_value, frequency_index, frequency_value = searchmaxima(maxima=maxima, data=input_data.T[j],distance=distance, scale_vector=freq_vec)
+        amp_values[j], frequency_indices[j], frequency_values[j] = searchmaxima(maxima=maxima, data=input_data.T[j],distance=distance, scale_vector=freq_vec)
 
         #print(any(frequency_value<freq_minimal_value))
-        if any(frequency_value<freq_minimal_value) == True:
+        if any(frequency_values<freq_minimal_value) == True:
             #print((frequency_value))
-            for i in range(len(frequency_index)):
+            for i in range(len(frequency_indices)):
 
 
-                if amp_value[i] > threshold:
-                    max_array[int(frequency_index[i]),j] = amp_value[i]
-    return max_array
+                if amp_values[i] > threshold:
+                    max_array[int(frequency_indices[i]),j] = amp_values[i]
+    return (max_array, amp_values, frequency_indices, frequency_values)
